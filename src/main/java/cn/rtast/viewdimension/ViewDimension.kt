ce3332a24ab1d673e8fca7790656947244564a94
RTAkland <rtakland@outlook.com>
2024-03-15 20:49:45 +0800

    update code

---
+++ src/main/java/cn/rtast/viewdimension/ViewDimension.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2024 RTAkland
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+
+package cn.rtast.viewdimension
+
+import net.fabricmc.api.ModInitializer
+import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.text.Text
+import net.minecraft.util.Formatting
+
+class ViewDimension : ModInitializer {
+
+    companion object {
+
+        fun replacePlayerName(player: ServerPlayerEntity): Text {
+            val dimension = player.world.dimension.effects.path
+            val oldName = player.name.string
+            val dimensionText = when (dimension) {
+                "overworld" -> Text.literal(" <Overworld>")
+                    .styled { it.withColor(Formatting.GREEN).withItalic(true) }
+                "the_nether" -> Text.literal(" <The Nether>")
+                    .styled { it.withColor(Formatting.DARK_RED).withItalic(true) }
+                "the_end" -> Text.literal(" <The End>")
+                    .styled { it.withColor(Formatting.DARK_PURPLE).withItalic(true) }
+                else -> Text.literal(" <$dimension>")
+                    .styled { it.withColor(Formatting.GRAY).withItalic(true) }
+            }
+            return Text.literal(oldName).append(dimensionText)
+        }
+
+    }
+
+    override fun onInitialize() {
+        println("ViewDimension loaded!")
+    }
+}
---
+++ src/main/java/cn/rtast/viewdimension/mixin/ServerPlayNetworkMixin.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2024 RTAkland
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+
+package cn.rtast.viewdimension.mixin;
+
+import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerPlayNetworkHandler.class)
+public abstract class ServerPlayNetworkMixin {
+
+    @Shadow
+    public ServerPlayerEntity player;
+
+    @Shadow public abstract void tick();
+
+    @Inject(method = "tick", at = @At("HEAD"))
+    public void tick(CallbackInfo ci) {
+        this.player.server
+                .getPlayerManager()
+                .sendToAll(new PlayerListS2CPacket(PlayerListS2CPacket.Action.UPDATE_DISPLAY_NAME, player));
+    }
+
+}
